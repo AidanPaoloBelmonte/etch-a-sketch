a33a274041bf62792d60a53884cc609496080834
AidanPaoloBelmonte <aidanpaolobelmonte@gmail.com>
2025-07-14 11:37:20 +0800

    Added more classes to generated boxes and formatting changes

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,29 @@
+let stylesheet = document.querySelector("style");
+
 let container = document.querySelector("#container");
 
-function initialize_grid() {
+function generateGrid(base) {
   if (container === null) {
     return;
   }
 
-  for (let l = 0; l < 16; l++) {
+  for (let l = 0; l < base * base; l++) {
     let ele = document.createElement("div");
-    ele.setAttribute("class", "box");
+    ele_id = l.toString();
+
+    ele.setAttribute("class", `box basis item${ele_id}`);
 
     container.appendChild(ele);
   }
 }
 
-initialize_grid();
+function changeFlexBasis(basis) {
+  if (stylesheet === null) {
+    console.error("Stylesheet was not fetched!");
+  }
+
+  stylesheet.removeRule(-1);
+  stylesheet.insertRule(`.basis { flex-basis: ${basis};}`, -1);
+}
+
+generateGrid(4);
