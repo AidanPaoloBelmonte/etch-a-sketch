377fe89b5ce91bed091fd8e10fc9a7c06772e3c4
AidanPaoloBelmonte <aidanpaolobelmonte@gmail.com>
2025-07-14 17:18:53 +0800

    Colors now start at low opacity and increase in opacity at repeated mouse hovers until the maximum

--- scripts/script.js
+++ scripts/script.js
@@ -8,11 +8,26 @@ let basis = 25;
 container.addEventListener("mouseover", (e) => {
   let target = e.target;
 
-  if (target.id === "container" || target.className == "box filled") {
+  if (target.id === "container") {
     return;
   }
 
-  let fillColor = randomColor();
+  let currentColor = target.style.backgroundColor;
+  let rgba = currentColor
+    .replace(/^(rgb|rgba)\(/, "")
+    .replace(/\)$/, "")
+    .replace(/\s/g, "")
+    .split(",");
+  let currentAlpha = 0.0;
+  if (rgba.length == 4) {
+    currentAlpha = parseFloat(rgba[3]);
+  } else if (rgba.length == 3) {
+    currentAlpha = 1.0;
+  }
+
+  currentAlpha = Math.min(currentAlpha + 0.12, 1.0);
+
+  let fillColor = randomColor(currentAlpha);
 
   target.setAttribute("class", "box filled");
   target.setAttribute(
@@ -58,13 +73,20 @@ function generateGrid(base) {
   }
 }
 
-function randomColor() {
-  let hex = "0123456789ABCDEF";
+function randomColor(opacity = 0) {
   let color = "#";
+  color += Math.floor(Math.random() * 16777215).toString(16);
 
-  for (let l = 0; l < 6; l++) {
-    color += hex[Math.floor(Math.random() * 16)];
+  if (color.length < 7) {
+    let missing = 7 - color.length;
+    for (let l = 0; l < missing; l++) {
+      color += "0";
+    }
   }
+
+  let opacityHex = Math.floor(opacity * 255).toString(16);
+
+  color += `${opacityHex}`;
 
   return color;
 }
