1eb0f3cc7ac40c431bff3954469f950afa425378
AidanPaoloBelmonte <aidanpaolobelmonte@gmail.com>
2025-07-14 15:27:10 +0800

    Box Items now change color on mouseover

--- scripts/script.js
+++ scripts/script.js
@@ -2,20 +2,38 @@ let stylesheet = document.styleSheets[0];
 
 let container = document.querySelector("#container");
 
+let basis = 25;
+
+container.addEventListener("mouseover", (e) => {
+  let target = e.target;
+
+  if (target.id === "container") {
+    return;
+  }
+
+  target.setAttribute(
+    "style",
+    `flex-basis: ${basis}%; background-color: black;`,
+  );
+});
+
 function generateGrid(base) {
   if (container === null) {
     return;
   }
 
+  basis = 100 / base;
+
   for (let l = 0; l < base * base; l++) {
     let ele = document.createElement("div");
     ele_id = l.toString();
 
-    ele.setAttribute("class", `box item${ele_id}`);
-    ele.setAttribute("style", `flex-basis: ${100 / base}%`);
+    ele.setAttribute("class", "box");
+    ele.setAttribute("id", `item-${l}`);
+    ele.setAttribute("style", `flex-basis: ${basis}%;`);
 
     container.appendChild(ele);
   }
 }
 
-generateGrid(25);
+generateGrid(4);
